206bbcbe408ceb8d167f90254b31b953621d1749
so-much-meta <38630665+so-much-meta@users.noreply.github.com>
2020-09-24 01:07:56 -0400

    Initial source commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Package setup script."""
+import setuptools
+
+setuptools.setup(
+    name='pypokereval',
+    version='0.1',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    extras_require={},
+    setup_requires=[],
+    tests_require=[],
+)
---
+++ src/pypokereval/__init__.py
@@ -0,0 +1,122 @@
+import gzip
+from array import array
+# import random
+# import time
+# import itertools
+
+
+HANDTYPES = [
+    "invalid hand",
+    "high card",
+    "one pair",
+    "two pairs",
+    "three of a kind",
+    "straight",
+    "flush",
+    "full house",
+    "four of a kind",
+    "straight flush"
+]
+
+CARDS = {
+    "2c": 1,
+    "2d": 2,
+    "2h": 3,
+    "2s": 4,
+    "3c": 5,
+    "3d": 6,
+    "3h": 7,
+    "3s": 8,
+    "4c": 9,
+    "4d": 10,
+    "4h": 11,
+    "4s": 12,
+    "5c": 13,
+    "5d": 14,
+    "5h": 15,
+    "5s": 16,
+    "6c": 17,
+    "6d": 18,
+    "6h": 19,
+    "6s": 20,
+    "7c": 21,
+    "7d": 22,
+    "7h": 23,
+    "7s": 24,
+    "8c": 25,
+    "8d": 26,
+    "8h": 27,
+    "8s": 28,
+    "9c": 29,
+    "9d": 30,
+    "9h": 31,
+    "9s": 32,
+    "tc": 33,
+    "td": 34,
+    "th": 35,
+    "ts": 36,
+    "jc": 37,
+    "jd": 38,
+    "jh": 39,
+    "js": 40,
+    "qc": 41,
+    "qd": 42,
+    "qh": 43,
+    "qs": 44,
+    "kc": 45,
+    "kd": 46,
+    "kh": 47,
+    "ks": 48,
+    "ac": 49,
+    "ad": 50,
+    "ah": 51,
+    "as": 52
+}
+
+INV_CARDS = {v: k for k, v in CARDS.items()}
+
+
+def gethand(cards, compress=False):
+    sep = ' ' if not compress else ''
+    return sep.join([INV_CARDS[c].capitalize() for c in cards])
+
+
+def eval7(cards):
+    p = 53
+    for c in cards:
+        p = ranks[p + c]
+    return p
+
+
+def eval5(cards):
+    p = 53
+    for c in cards:
+        p = ranks[p + c]
+    p = ranks[p]
+    return p
+
+
+eval6 = eval5
+
+
+def evalany(cards):
+    p = 53
+    for c in cards:
+        p = ranks[p + c]
+    if 5 <= len(cards) <= 6:
+        p = ranks[p]
+    return p
+
+
+def getrank(p):
+    return {
+        "handType": p >> 12,
+        "handRank": p & 0x00000fff,
+        "value": p,
+        "handName": HANDTYPES[p >> 12]
+    }
+
+
+with gzip.open('./HandRanks.dat.gz') as f:
+    ranks = array('I')
+    ranks.fromfile(f, 32487834)
