0ad78123c37f5470ba81c5e30dfca1d088a5e292
so-much-meta <38630665+so-much-meta@users.noreply.github.com>
2020-09-25 00:17:32 -0400

    rename

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 import setuptools
 
 setuptools.setup(
-    name='pypokereval',
+    name='pypokah',
     version='0.1',
     packages=setuptools.find_packages('src'),
-    package_data={'pypokereval': ['data/HandRanks.dat.gz']},
+    package_data={'pypokah': ['data/HandRanks.dat.gz']},
     package_dir={'': 'src'},
     install_requires=[],
     extras_require={},
