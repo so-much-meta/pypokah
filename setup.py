ad213027923b8861d3f253d5b8ab0217f16c34c0
so-much-meta <38630665+so-much-meta@users.noreply.github.com>
2020-09-26 00:13:30 -0400

    Update handranks data package_data

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     name='pypokah',
     version='0.1',
     packages=setuptools.find_packages('src'),
-    package_data={'pypokah': ['data/HandRanks.dat.gz']},
+    package_data={'pypokah': ['data/*.dat.lzma']},
     package_dir={'': 'src'},
     install_requires=[],
     extras_require={},
